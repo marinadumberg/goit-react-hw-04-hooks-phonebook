15676cbda3e4c5fce7cb6f6984f467415f0e3eab
marinadumberg <marina.dumberg@inbox.lv>
2021-07-29 18:15:10 +0300

    updateSrc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Section from "./components/Section/Section";
+import Form from "./components/Form/Form";
+import ContactsList from "./components/ContactsList/ContactsList";
+import Filter from "./components/Filter/Filter";
+import { v4 as unId } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  isInList = (contact, contacts) =>
+    contacts.find((cont) =>
+      cont.name.toLocaleLowerCase().includes(contact.name.toLocaleLowerCase())
+    );
+  addContact = (e) => {
+    const { contacts } = this.state;
+    console.log({ contacts });
+    const contact = {
+      id: unId(),
+      name: e.name,
+      number: e.number,
+    };
+
+    this.isInList(contact, contacts)
+      ? alert(`${contact.name} is already in your list`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((cont) => cont.id !== contactId),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  componentDidMount() {
+    // console.log('didmount');
+    const contacts = localStorage.getItem("contacts");
+    // console.log(contacts);
+    const parsedContacts = JSON.parse(contacts);
+    // console.log(parsedContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const normalizeContacts = this.state.filter.toLocaleLowerCase();
+    const visibleContacts = this.state.contacts.filter((contact) =>
+      contact.name.toLocaleLowerCase().includes(normalizeContacts)
+    );
+    const { filter } = this.state;
+    return (
+      <div>
+        <Section title="Phonebook">
+          <Form onSubmit={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactsList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./ContactsList.module.css";
+
+const ContactsList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={s.contactList}>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id} className={s.contactItem}>
+          <p>
+            {name},{number}
+          </p>
+          <button
+            className={s.deleteBtn}
+            type="button"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactsList;
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
